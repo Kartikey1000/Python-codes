92456cccff6cc3906c67019af6f46b59c9d33e50
kartikey <ks632794@gmail.com>
2026-09-19 12:58:58 +0530

    Create image.py

---
+++ image.py
@@ -0,0 +1,18 @@
+from PIL import Image
+from PIL import ImageFilter
+
+def main():
+    with Image.open("in.jpg") as img:
+        #print(img.size)
+        #print(img.format)
+        #img= img.rotate(180)
+        #img=img.rotate(180)
+        #img.save("out.jpeg")
+        #img=img.filter(ImageFilter.BLUR)
+        img=img.filter(ImageFilter.FIND_EDGES)
+
+        img.save("out.jpeg")
+
+
+
+main()
