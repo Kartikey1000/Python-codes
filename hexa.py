70b3333eaad8bed089aea99a581f078613718a0c
kartikey <ks632794@gmail.com>
2026-09-21 15:47:40 +0530

    Create hexa.py

---
+++ hexa.py
@@ -0,0 +1,19 @@
+import re 
+
+def main():
+    code=input("Hexadecimal color code: ")
+
+    pattern= r"^#[a-fA-F0-9]{6}$"
+    match = re.search(pattern,code)
+    if match:
+        print(f"valid. match with {match.group()}")
+    else:
+        print("Invalid")
+
+
+
+
+
+
+
+main()
