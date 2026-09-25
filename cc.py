c438373da5cd178569c3897482beceecbf634c9d
kartikey <ks632794@gmail.com>
2026-09-25 20:19:50 +0530

    Create cc.py

---
+++ cc.py
@@ -0,0 +1,13 @@
+"""def f(*args,**kwargs):
+    print("Named:",kwargs)
+
+f(galleons=100,sickles=50,knuts=25)"""
+
+def main():
+    yell("tHis is CS")
+
+def yell(phrase):
+    print(phrase.upper())
+
+if __name__=="__main__":
+    main()
